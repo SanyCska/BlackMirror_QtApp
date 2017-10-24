0047e825f8cf9b15bfcece938c5588822feb67c3
SanyCska <alexander2302@mail.ru>
2017-10-25 02:21:17 +0300

    it is a small step for a project and the giant for our activity

---
+++ MainWindow.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+
+    #данная функция по большей части генерится из файла ui, созданного в qt_designer
+    #строчки, дописанные отдельно будут прокомментированы
+    def setupUi(self, MainWindow):
+
+        MainWindow.setObjectName("MainWindow")
+        #растягиваем окно на весь экран
+        MainWindow.setWindowState(QtCore.Qt.WindowMaximized)
+        #устанавливаем начальный цвет фона
+        MainWindow.setStyleSheet("background-color: rgb(128,128,128);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        #устанавливаем цвет кнопки
+        self.pushButton_2.setStyleSheet("background-color: 	rgb(255,255,255);")
+        self.gridLayout.addWidget(self.pushButton_2, 2, 1, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 0, 1, 1, 1)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem1, 2, 2, 1, 1)
+        spacerItem2 = QtWidgets.QSpacerItem(340, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem2, 1, 0, 1, 1)
+        spacerItem3 = QtWidgets.QSpacerItem(339, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem3, 1, 2, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        # устанавливаем цвет кнопки
+        self.pushButton.setStyleSheet("background-color: 	rgb(255,255,255);")
+        self.gridLayout.addWidget(self.pushButton, 1, 1, 1, 1)
+        spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem4, 2, 0, 1, 1)
+        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem5, 3, 1, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 27))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        #вызываем функцию, которая скрывает одну из кнопок в начале
+        self.hide_layout()
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        #При нажатии pushButton появляется вторая кнопка и меняется фон окна
+        self.pushButton.pressed.connect(self.show_layout)
+        self.pushButton.pressed.connect(self.background_change)
+
+    #скрываем кнопку
+    def hide_layout(self):
+        self.pushButton_2.hide()
+
+    #покаываем кнопку
+    def show_layout(self):
+        self.pushButton_2.show()
+
+    #меняем цвет фона
+    def background_change(self):
+        MainWindow.setStyleSheet("background-color: rgb(192,192,192);")
+
+#с помощью этого кода запускаем всю программу
+#просто поверьте
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
