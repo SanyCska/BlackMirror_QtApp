e8cd33ab2d596416e8b211a052ea9a198f24f873
SanyCska <alexander2302@mail.ru>
2017-11-11 00:48:35 +0300

    added second thread for client-server connection

--- FullWeatherAPI.py
+++ FullWeatherAPI.py
@@ -1,16 +1,29 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from weather import Weather
+from threading import Thread #Потоки
+from PyQt5.QtCore import QObject, pyqtSignal
+from socket import *
 import sys
 
+HOST = 'localhost'  # адрес хоста (сервера) пустой означает использование любого доступного адреса
+PORT = 21110  # номер порта на котором работает сервер (от 0 до 65525, порты до 1024 зарезервированы для системы, порты TCP и UDP не пересекаются)
+BUFSIZ = 1024  # размер буфера 1Кбайт
+ADDR = (HOST, PORT)  # адрес сервера
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)  # связываем сокет с адресом
+tcpSerSock.listen(5)  # устанавливаем максимальное число клиентов одновременно обслуживаемых
+
 class Ui_Form(object):
+
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.setStyleSheet('background-color: black')
-        Form.resize(1167, 912)
+        # Form.resize(1167, 912)
+        MainWindow.setWindowState(QtCore.Qt.WindowMaximized)
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(860, 110, 181, 181))
+        self.label.setGeometry(QtCore.QRect(360, 110, 181, 181))
         self.label.setText("")
-        self.label.setPixmap(QtGui.QPixmap("../../Downloads/Pic/Cloud.jpg"))
+        self.label.setPixmap(QtGui.QPixmap("Cloud.jpg"))
         self.label.setObjectName("label")
         self.label.hide()
 
@@ -21,8 +34,9 @@ class Ui_Form(object):
         self.lcdNumber.setObjectName("lcdNumber")
         self.hide_lcd()
 
+
         self.textEdit = QtWidgets.QTextEdit(Form)
-        self.textEdit.setGeometry(QtCore.QRect(840, 300, 261, 200))
+        self.textEdit.setGeometry(QtCore.QRect(340, 300, 261, 200))
         self.textEdit.setReadOnly(True)
         self.textEdit.setStyleSheet('background-color: black;'
                                     'border-style: solid;'
@@ -33,28 +47,28 @@ class Ui_Form(object):
 
         self.pushButton = QtWidgets.QPushButton(Form)
         self.pushButton.setStyleSheet('background-color: grey')
-        self.pushButton.setGeometry(QtCore.QRect(10, 850, 150, 46))
+        self.pushButton.setGeometry(QtCore.QRect(100, 550, 150, 46))
         self.pushButton.setObjectName("pushButton")
         self.pushButton.pressed.connect(self.show_all)
 
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(860, 110, 181, 181))
+        self.label_2.setGeometry(QtCore.QRect(360, 110, 181, 181))
         self.label_2.setText("")
-        self.label_2.setPixmap(QtGui.QPixmap("../../Downloads/Pic/Rain.jpg"))
+        self.label_2.setPixmap(QtGui.QPixmap("Rain.jpg"))
         self.label_2.setObjectName("label_2")
         self.label_2.hide()
 
         self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setGeometry(QtCore.QRect(860, 110, 181, 181))
+        self.label_3.setGeometry(QtCore.QRect(360, 110, 181, 181))
         self.label_3.setText("")
-        self.label_3.setPixmap(QtGui.QPixmap("../../Downloads/Pic/Sun.jpg"))
+        self.label_3.setPixmap(QtGui.QPixmap("Sun.jpg"))
         self.label_3.setObjectName("label_3")
         self.label_3.hide()
 
         self.label_4 = QtWidgets.QLabel(Form)
-        self.label_4.setGeometry(QtCore.QRect(860, 110, 181, 181))
+        self.label_4.setGeometry(QtCore.QRect(360, 110, 181, 181))
         self.label_4.setText("")
-        self.label_4.setPixmap(QtGui.QPixmap("../../Downloads/Pic/Snow.jpg"))
+        self.label_4.setPixmap(QtGui.QPixmap("Snow.jpg"))
         self.label_4.setObjectName("label_4")
         self.label_4.hide()
 
@@ -72,13 +86,18 @@ class Ui_Form(object):
     def hide_text(self):
         self.textEdit.hide()
 
+    def city(self, string):
+        self.city = string
+        print(self.city)
+
     def show_all(self):
         weather = Weather()
         location = weather.lookup_by_location('Moscow')
         condition = location.condition()
-        temp = (int(condition['temp']) - 32) * 5 // 9
+        temp = (int(condition.temp()) - 32) * 5 // 9
         self.lcdNumber.display(temp)
-        text = str(condition['text'])
+        text = str(condition.text())
+        print(text)
         if 'loudy' in text:
             self.label.show()
         else:
@@ -93,10 +112,31 @@ class Ui_Form(object):
                     else:
                         if 'hover' in text:
                             self.label_2.show
-
         self.textEdit.setText(text)
         self.lcdNumber.show()
         self.textEdit.show()
+
+class MyThread(Thread):
+    def __init__(self, f):
+        Thread.__init__(self)
+        self.f = f
+
+    def run(self):
+        tcpCliSock, addr = tcpSerSock.accept()
+        print('Connected from: {}'.format(addr))
+        while True:
+            data = tcpCliSock.recv(BUFSIZ)
+            data.decode('utf8')
+            if data:
+                print(data)
+                if data == b'1':
+                    self.f.message.emit()
+                if data == b'Moscow':
+                    self.f.message2.emit('Moscow')
+
+class foo(QObject):
+    message = pyqtSignal()
+    message2 = pyqtSignal(str)
 
 
 if __name__ == "__main__":
@@ -106,4 +146,9 @@ if __name__ == "__main__":
     ui = Ui_Form()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    f = foo()
+    f.message.connect(ui.show_all)
+    f.message2.connect(ui.city)
+    t = MyThread(f)
+    t.start()
     sys.exit(app.exec_())
